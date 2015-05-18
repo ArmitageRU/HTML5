de5284bf050c7cbb72502d4245ea80a9d36b8357
ArmitageRU <ronzhist@gmail.com>
2015-05-18 18:43:24 +0300

    mouse pressed

--- js/Planet.js
+++ js/Planet.js
@@ -14,9 +14,14 @@ Planet.prototype = {
         this.position.y = this.orbit.centre.y + this.orbit.radius * Math.sin(this.angle);
         this.angle += this.speed * deltaTime*0.001;
 		var ctx = this.orbit.ctx;
-        ctx.lineWidth = 1;
-		if(this.selected)ctx.strokeStyle = 'white';
-		else ctx.strokeStyle = 'blue';
+		if(this.selected){	
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = 'white';
+		}
+		else {
+			ctx.strokeStyle = 'blue';
+			ctx.lineWidth = 1;
+		}
         ctx.beginPath();
         ctx.arc(this.position.x, this.position.y, 25, 0, Math.PI * 2, true);
 		ctx.fillStyle = 'blue';
