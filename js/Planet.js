279fcb825c6f4720942f39cdfe6a1d7bbee64c99
ArmitageRU <ronzhist@gmail.com>
2015-05-18 15:43:38 +0300

    qqq

--- js/Planet.js
+++ js/Planet.js
@@ -2,7 +2,7 @@
 function Planet(title){
 	this.position = new Point(0,0);
 	this.angle = ~~(Math.random()*360);
-	this.speed = Math.random()*0.00001+0.00001;
+	this.speed = 1/~~(Math.random()*240);//Math.random()*0.00001+0.00001;
 	this.title = title;
 	this.orbit;
 };
@@ -11,7 +11,7 @@ Planet.prototype = {
 	Draw: function(deltaTime){
 		this.position.x = this.orbit.centre.x + this.orbit.radius * Math.cos(this.angle);
         this.position.y = this.orbit.centre.y + this.orbit.radius * Math.sin(this.angle);
-        this.angle += this.speed * deltaTime;
+        this.angle += this.speed * deltaTime*0.001;
 		var ctx = this.orbit.ctx;
         ctx.lineWidth = 1;
 		ctx.strokeStyle = 'blue';
